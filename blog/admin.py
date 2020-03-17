2d7d8b73aaad037bcc379e13d86dfa964b88e810
maohanghang19930504@gmail.com <maohanghang19930504@gmail.com>
2020-03-17 18:53:50 +0800

    qq

---
+++ blog/admin.py
@@ -0,0 +1,123 @@
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+# Register your models here.
+from .models import Post, Category, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    fields = ('name', 'status', 'is_nav')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time',
+        # 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+    #   编辑界面
+    save_on_top = True
+
+    exclude = ('owner',)
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+           'fields': (
+               'desc',
+               'content',
+           )
+        }),
+        ('额外信息', {
+            'classes': ('collapse', ),
+            'fields': ('tag', ),
+        })
+    )
+
+    # filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
+
+    # def operator(self, obj):
+    #     return format_html(
+    #         '<a href="{}">编辑</a>',
+    #         reverse('admin.blog_post_change', args=(obj.id, ))
+    #     )
+    # operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    class Media:
+        css = {
+            'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js')
+
+    # def get_media(self):
+    #     # xadmin基于bootstrap，引入会页面样式冲突，仅供参考, 故注释。
+    #     media = super().get_media()
+    #     media.add_js(['https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js'])
+    #     media.add_css({
+    #     'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     })
+    #     return media
